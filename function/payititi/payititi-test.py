6b3e42a075f3076d673c9263b10f76e5e08ff8e4
qinlicang <qinlicang@gmail.com>
2023-08-04 15:41:32 +0800

    modify

--- function/payititi/payititi-test.py
+++ function/payititi/payititi-test.py
@@ -5,13 +5,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
-# chrome_options = Options()
-# chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
-# chrome_options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
-# chrome_options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
-# chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
-
-
 if __name__ == "__main__":
     caps = webdriver.DesiredCapabilities.CHROME.copy() 
     caps['acceptInsecureCerts'] = True
