c642a126c32b83582d76842b37b860706dbdacc5
qinlicang <qinlicang@gmail.com>
2023-08-04 17:35:36 +0800

    delete desired_capabilities

--- function/payititi/payititi-test.py
+++ function/payititi/payititi-test.py
@@ -6,9 +6,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 if __name__ == "__main__":
-    caps = webdriver.DesiredCapabilities.CHROME.copy() 
-    caps['acceptInsecureCerts'] = True
-    driver = webdriver.Chrome(desired_capabilities=caps)    
+    # caps = webdriver.DesiredCapabilities.CHROME.copy() 
+    # caps['acceptInsecureCerts'] = True
+    # driver = webdriver.Chrome(desired_capabilities=caps)    
     driver = webdriver.Chrome()
     print('selenium load web driver successfully')
     driver.get('https://www.payititi.com')
