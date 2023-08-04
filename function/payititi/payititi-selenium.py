1c91313a86a6f547eabf7f629c4d3d738b6e306a
qinlicang <qinlicang@gmail.com>
2023-08-04 11:12:27 +0800

    Update payititi-selenium.py

--- function/payititi/payititi-selenium.py
+++ function/payititi/payititi-selenium.py
@@ -4,6 +4,7 @@
 import json,time,os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import UnexpectedAlertPresentException
 from selenium.common.exceptions import NoAlertPresentException
 from datetime import datetime
@@ -13,7 +14,7 @@ import requests
 # from selenium.webdriver.support.ui import WebDriverWait
 # from selenium.webdriver.support import expected_conditions as EC
 
-chrome_options = webdriver.ChromeOptions()
+chrome_options = Options()
 chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
 chrome_options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
 chrome_options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
@@ -22,7 +23,7 @@ chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. l
 def get_web_driver():
     chromedriver = "/usr/bin/chromedriver"
     os.environ["webdriver.chrome.driver"] = chromedriver
-    driver = webdriver.Chrome(executable_path=chromedriver, chrome_options=chrome_options)
+    driver = webdriver.Chrome(executable_path=chromedriver, options=chrome_options)
     driver.implicitly_wait(10) # 所有的操作都可以最长等待10s
     return driver
 
@@ -115,4 +116,3 @@ if __name__ == "__main__":
     # sendKey = "SCT218351TZ85R81jSICq5lvWiMK7RsWdq"
 
     autoSign(sendKey, user, password)
-    
