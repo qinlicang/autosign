f7d2794da1bc18d2945c8cd7c439ad8a2adf944c
qinlicang <qinlicang@gmail.com>
2023-08-04 17:52:59 +0800

    set chrome_options

--- function/payititi/payititi-test.py
+++ function/payititi/payititi-test.py
@@ -6,10 +6,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 
 if __name__ == "__main__":
-    # caps = webdriver.DesiredCapabilities.CHROME.copy() 
-    # caps['acceptInsecureCerts'] = True
-    # driver = webdriver.Chrome(desired_capabilities=caps)    
-    driver = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    chrome_options.add_argument('--no-sandbox') # 解决DevToolsActivePort文件不存在的报错
+    chrome_options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
+    chrome_options.add_argument('--disable-gpu') # 谷歌文档提到需要加上这个属性来规避bug
+    chrome_options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+
+    chromedriver = "/usr/bin/chromedriver"
     print('selenium load web driver successfully')
+    driver = webdriver.Chrome(executable_path=chromedriver, options=chrome_options)
     driver.get('https://www.payititi.com')
     print(f'selenium load payititi page:{driver.title}')
