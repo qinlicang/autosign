0450521bb82555dc841a52050cf05fabe82f8906
qinlicang <qinlicang@gmail.com>
2023-08-07 16:29:01 +0800

    used ui

--- function/payititi/payititi-selenium.py
+++ function/payititi/payititi-selenium.py
@@ -20,7 +20,7 @@ options.add_argument('--disable-dev-shm-usage') # 在某些VM环境中，/dev/sh
 options.add_argument('--no-sandbox') # 沙盒模式运行 对通常为沙盒的所有进程类型禁用沙盒。
 options.add_argument('window-size=1920x1080') # 指定浏览器分辨率
 options.add_argument('--disable-gpu') # 禁用GPU加速 谷歌文档提到需要加上这个属性来规避bug
-options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
+# options.add_argument('--headless') # 浏览器不提供可视化页面. linux下如果系统不支持可视化不加这条会启动失败
 options.add_argument('--hide-scrollbars') #隐藏滚动条, 应对一些特殊页面
 options.add_argument("–no-first-run") # 初始化时为空白页面
 # options.add_argument('blink-settings=imagesEnabled=false') #不加载图片, 提升速度
@@ -68,10 +68,10 @@ def autoSign(sendKey, user, password):
         # browser.find_element(By.NAME, 'submit').click()
         loginButton = browser.find_element(By.XPATH, '//*[@id="signin-form"]/div[3]/form/input[4]')
         loginButton.click()
-        time.sleep(5)
-        print(f'loginButton:{loginButton.get_attribute("innerHTML")}')
-        loginDiv = browser.find_element(By.XPATH, '//*[@id="signin-form"]')
-        print(f'loginDiv:{loginDiv.get_attribute("innerHTML")}')
+        # time.sleep(5)
+        # print(f'loginButton:{loginButton.get_attribute("innerHTML")}')
+        # loginDiv = browser.find_element(By.XPATH, '//*[@id="signin-form"]')
+        # print(f'loginDiv:{loginDiv.get_attribute("innerHTML")}')
 
         print(f'selenium logined page title:{browser.title}')
 
